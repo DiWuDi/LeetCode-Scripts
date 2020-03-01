cd2846949386bf56af77855a7d70168d1eded20a
DiwuDi <dwzoon@gmail.com>
2020-03-01 12:07:11 +0800

    added #66 digits plus one--this is smart!

--- 66_plusOne.py
+++ 66_plusOne.py
@@ -4,9 +4,24 @@ The digits are stored such that the most significant digit is at the head of the
  array contain a single digit.
 You may assume the integer does not contain any leading zero, except the number 0 itself."""
 
-class Solution(object):
+class Solution:
     def plusOne(self, digits):
         """
         :type digits: List[int]
         :rtype: List[int]
         """
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:  #keep on checking if the current is 9
+                digits[i] = 0
+            else: 
+                digits[i] += 1
+                return digits  # return if not all 9
+        digits[0] = 1
+        digits.append(0)
+        return digits
+
+
+
+test = Solution()
+str_ = [9,8,9]
+print(test.plusOne(str_))
